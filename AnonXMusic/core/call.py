19598e2fe02e7f5879d160fcd3356eec7e27423d
Champu <112340896+ChampuXD@users.noreply.github.com>
2024-05-11 23:14:11 +0530

    dasd

--- AnonXMusic/core/call.py
+++ AnonXMusic/core/call.py
@@ -7,7 +7,6 @@ from pytgcalls import filters
 from pytgcalls.types import ChatUpdate
 from pyrogram import Client
 from pytgcalls import *
-from pytgcalls import AudioPiped, AudioVideoPiped
 from pytgcalls.types import MediaStream
 from pyrogram.types import InlineKeyboardMarkup
 from pytgcalls.exceptions import (
